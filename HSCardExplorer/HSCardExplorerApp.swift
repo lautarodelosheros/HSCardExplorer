70fdc15e085357f69e2dce26c7c05745a6d4ef97
Lautaro de los Heros <lautarodelosheros@gmail.com>
2022-11-01 23:40:01 -0300

    Initial Commit

---
+++ HSCardExplorer/HSCardExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSCardExplorerApp.swift
+//  HSCardExplorer
+//
+//  Created by Lautaro de los Heros on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct HSCardExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HSCardExplorerUITests/HSCardExplorerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HSCardExplorerUITestsLaunchTests.swift
+//  HSCardExplorerUITests
+//
+//  Created by Lautaro de los Heros on 01/11/2022.
+//
+
+import XCTest
+
+final class HSCardExplorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
